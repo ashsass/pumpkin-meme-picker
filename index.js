b162177b0ca86c3559874259369c9a1fb740e8a8
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-03-02 13:23:04 -0500

    Added selectors for styling highlighted options

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 import { catsData } from "./data.js";
 const emotionRadios = document.getElementById('emotion-radios')
+const imageButton = document.getElementById('get-image-btn')
+
+/*
+Challenge:
+1. Set up an eventlistener which calls a new
+   function called "getMatchingCatsArray" when
+   the "Get Image" button is clicked.
+2. getMatchingCatsArray should save the value
+   of the checked radio input to a const and 
+   log out that const.
+*/
+
+imageButton.addEventListener('click', getMatchingCatsArray)
+
+function getMatchingCatsArray() {
+    const checkedRadio = document.querySelector('input[type="radio"]:checked')
+    console.log(checkedRadio.value)
+}
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+
+function highlightCheckedOption(e){
+    const radioItems = document.getElementsByClassName('radio')
+    for (let item of radioItems){
+        item.classList.remove("highlight")
+    } //Clears all highlight field before moving on to another radio item
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
 
 function getEmotionsArray(cats){
     const emotionArray = [];
